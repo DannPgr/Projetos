a33c7cb2261637138de06292bcf34098f01c0aac
DannPgr <202504543181@alunos.estacio.br>
2025-05-07 14:37:51 +0000

    git add <olamundo.c> git commit -m "Olá Mundo" git push

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(){
+    int soma, num1, num2;
+
+    printf("Digite um número:\n");
+    scanf("%d",&num1);
+
+    printf("Digite outro número:\n");
+    scanf("%d",&num2);
+    
+    soma = num1 * num2;
+
+    printf("A soma de %d e %d é: %d.\n",num1,num2,soma);
+
+    return 0;
+
+}
